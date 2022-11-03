13a29f90374a42621a49b36a6461dc0666e5f091
enderta <endertanver@gmail.com>
2022-11-03 15:38:04 +0000

    car

--- src/App.js
+++ src/App.js
@@ -2,37 +2,7 @@ import React from 'react';
 import MainLib from "./compononts/lib/MainLib";
 
 const App = () => {
-    const [users, setUsers] = React.useState([]);
-    const [search, setSearch] = React.useState('');
-    const [filteredUsers, setFilteredUsers] = React.useState([]);
 
-    React.useEffect(() => {
-        const myHeaders = new Headers();
-        myHeaders.append("x-library-token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImlkIjoiNTgzNiIsImZ1bGxfbmFtZSI6IlRlc3QgTGlicmFyaWFuIDI0IiwiZW1haWwiOiJsaWJyYXJpYW4yNEBsaWJyYXJ5IiwidXNlcl9ncm91cF9pZCI6IjIifSwiaWF0IjoxNjY3Mzg3NjA1LCJleHAiOjE2Njk5Nzk2MDV9.P8Y0XKzwiZjRA8718Q3pGOaTodEI5rGi9KL_SUbhrm0");
-
-        const requestOptions = {
-            method: 'GET',
-            headers: myHeaders,
-            redirect: 'follow'
-        };
-
-        fetch("https://library2.cydeo.com/rest/v1/get_users?user_group=null&user_status=ACTIVE&draw=2&columns[0][data]=actions&columns[0][name]=actions&columns[0][searchable]=true&columns[0][orderable]=false&columns[0][search][value]=&columns[0][search][regex]=false&columns[1][data]=id&columns[1][name]=id&columns[1][searchable]=true&columns[1][orderable]=true&columns[1][search][value]=&columns[1][search][regex]=false&columns[2][data]=full_name&columns[2][name]=full_name&columns[2][searchable]=true&columns[2][orderable]=true&columns[2][search][value]=&columns[2][search][regex]=false&columns[3][data]=email&columns[3][name]=email&columns[3][searchable]=true&columns[3][orderable]=true&columns[3][search][value]=&columns[3][search][regex]=false&columns[4][data]=group_name&columns[4][name]=group_name&columns[4][searchable]=true&columns[4][orderable]=true&columns[4][search][value]=&columns[4][search][regex]=false&columns[5][data]=status&columns[5][name]=status&columns[5][searchable]=true&columns[5][orderable]=true&columns[5][search][value]=&columns[5][search][regex]=false&order[0][column]=1&order[0][dir]=desc&start=0&length=500&search[value]=&search[regex]=false&_=1667389886463", requestOptions)
-            .then(response => response.json())
-            .then(result => {
-                setUsers(result.data);
-                setFilteredUsers(result.data);
-            })
-            .catch(error => console.log('error', error));
-    }, []);
-    console.log(users);
-    const handleSearch = (event) => {
-        setSearch(event.target.value);
-        setFilteredUsers(
-            users.filter((user) => {
-                return user.full_name.toLowerCase().includes(event.target.value.toLowerCase());
-            })
-        );
-    }
     return (
         <div>
         <MainLib/>
--- src/compononts/lib/Users.jsx
+++ src/compononts/lib/Users.jsx
@@ -1,6 +1,37 @@
 import React from 'react';
 
 const Users = () => {
+    const [users, setUsers] = React.useState([]);
+    const [search, setSearch] = React.useState('');
+    const [filteredUsers, setFilteredUsers] = React.useState([]);
+
+    React.useEffect(() => {
+        const myHeaders = new Headers();
+        myHeaders.append("x-library-token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImlkIjoiNTgzNiIsImZ1bGxfbmFtZSI6IlRlc3QgTGlicmFyaWFuIDI0IiwiZW1haWwiOiJsaWJyYXJpYW4yNEBsaWJyYXJ5IiwidXNlcl9ncm91cF9pZCI6IjIifSwiaWF0IjoxNjY3Mzg3NjA1LCJleHAiOjE2Njk5Nzk2MDV9.P8Y0XKzwiZjRA8718Q3pGOaTodEI5rGi9KL_SUbhrm0");
+
+        const requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+
+        fetch("https://library2.cydeo.com/rest/v1/get_users?user_group=null&user_status=ACTIVE&draw=2&columns[0][data]=actions&columns[0][name]=actions&columns[0][searchable]=true&columns[0][orderable]=false&columns[0][search][value]=&columns[0][search][regex]=false&columns[1][data]=id&columns[1][name]=id&columns[1][searchable]=true&columns[1][orderable]=true&columns[1][search][value]=&columns[1][search][regex]=false&columns[2][data]=full_name&columns[2][name]=full_name&columns[2][searchable]=true&columns[2][orderable]=true&columns[2][search][value]=&columns[2][search][regex]=false&columns[3][data]=email&columns[3][name]=email&columns[3][searchable]=true&columns[3][orderable]=true&columns[3][search][value]=&columns[3][search][regex]=false&columns[4][data]=group_name&columns[4][name]=group_name&columns[4][searchable]=true&columns[4][orderable]=true&columns[4][search][value]=&columns[4][search][regex]=false&columns[5][data]=status&columns[5][name]=status&columns[5][searchable]=true&columns[5][orderable]=true&columns[5][search][value]=&columns[5][search][regex]=false&order[0][column]=1&order[0][dir]=desc&start=0&length=500&search[value]=&search[regex]=false&_=1667389886463", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                setUsers(result.data);
+                setFilteredUsers(result.data);
+            })
+            .catch(error => console.log('error', error));
+    }, []);
+    console.log(users);
+    const handleSearch = (event) => {
+        setSearch(event.target.value);
+        setFilteredUsers(
+            users.filter((user) => {
+                return user.full_name.toLowerCase().includes(event.target.value.toLowerCase());
+            })
+        );
+    }
     return (
         <div>
             <section id="users" data-load="users.html" className="spapp-created">
@@ -85,6 +116,8 @@ const Users = () => {
                                             className="form-control input-sm input-small input-inline"
                                             placeholder=""
                                             aria-controls="tbl_users"
+                                            value={search}
+                                            onChange={handleSearch}
                                         />
                                     </label>
                                 </div>
@@ -184,186 +217,37 @@ const Users = () => {
                                 </tr>
                                 </thead>
                                 <tbody>
-                                <tr role="row" className="odd">
-                                    <td>
-                                        <a
-                                            onClick="Users.edit_user(7051)"
-                                            href="javascript:void(0)"
-                                            className="btn btn-primary btn-sm"
-                                            role="button"
-                                        >
-                                            <i className="fa fa-pencil-square-o" aria-hidden="true"/>{" "}
-                                            Edit User
-                                        </a>
-                                    </td>
-                                    <td>7051</td>
-                                    <td>Sima Gleichner</td>
-                                    <td>nelia.volkman@hotmail.com</td>
-                                    <td>Students</td>
-                                    <td>ACTIVE</td>
-                                </tr>
-                                <tr role="row" className="even">
-                                    <td>
-                                        <a
-                                            onClick="Users.edit_user(7050)"
-                                            href="javascript:void(0)"
-                                            className="btn btn-primary btn-sm"
-                                            role="button"
-                                        >
-                                            <i className="fa fa-pencil-square-o" aria-hidden="true"/>{" "}
-                                            Edit User
-                                        </a>
-                                    </td>
-                                    <td>7050</td>
-                                    <td>Lavonne Cole</td>
-                                    <td>vernice.klein@hotmail.com</td>
-                                    <td>Librarian</td>
-                                    <td>ACTIVE</td>
-                                </tr>
-                                <tr role="row" className="odd">
-                                    <td>
-                                        <a
-                                            onClick="Users.edit_user(7049)"
-                                            href="javascript:void(0)"
-                                            className="btn btn-primary btn-sm"
-                                            role="button"
-                                        >
-                                            <i className="fa fa-pencil-square-o" aria-hidden="true"/>{" "}
-                                            Edit User
-                                        </a>
-                                    </td>
-                                    <td>7049</td>
-                                    <td>Maris Leannon</td>
-                                    <td>jefferey.ruecker@yahoo.com</td>
-                                    <td>Students</td>
-                                    <td>ACTIVE</td>
-                                </tr>
-                                <tr role="row" className="even">
-                                    <td>
-                                        <a
-                                            onClick="Users.edit_user(7048)"
-                                            href="javascript:void(0)"
-                                            className="btn btn-primary btn-sm"
-                                            role="button"
-                                        >
-                                            <i className="fa fa-pencil-square-o" aria-hidden="true"/>{" "}
-                                            Edit User
-                                        </a>
-                                    </td>
-                                    <td>7048</td>
-                                    <td>Mrs. Tosha Cummings</td>
-                                    <td>willian.gottlieb@yahoo.com</td>
-                                    <td>Librarian</td>
-                                    <td>ACTIVE</td>
-                                </tr>
-                                <tr role="row" className="odd">
-                                    <td>
-                                        <a
-                                            onClick="Users.edit_user(7047)"
-                                            href="javascript:void(0)"
-                                            className="btn btn-primary btn-sm"
-                                            role="button"
-                                        >
-                                            <i className="fa fa-pencil-square-o" aria-hidden="true"/>{" "}
-                                            Edit User
-                                        </a>
-                                    </td>
-                                    <td>7047</td>
-                                    <td>Ariel Dooley</td>
-                                    <td>leopoldo.johnston@yahoo.com</td>
-                                    <td>Students</td>
-                                    <td>ACTIVE</td>
-                                </tr>
-                                <tr role="row" className="even">
-                                    <td>
-                                        <a
-                                            onClick="Users.edit_user(7046)"
-                                            href="javascript:void(0)"
-                                            className="btn btn-primary btn-sm"
-                                            role="button"
-                                        >
-                                            <i className="fa fa-pencil-square-o" aria-hidden="true"/>{" "}
-                                            Edit User
-                                        </a>
-                                    </td>
-                                    <td>7046</td>
-                                    <td>Chere Mayer</td>
-                                    <td>roselyn.jerde@hotmail.com</td>
-                                    <td>Librarian</td>
-                                    <td>ACTIVE</td>
-                                </tr>
-                                <tr role="row" className="odd">
-                                    <td>
-                                        <a
-                                            onClick="Users.edit_user(7045)"
-                                            href="javascript:void(0)"
-                                            className="btn btn-primary btn-sm"
-                                            role="button"
-                                        >
-                                            <i className="fa fa-pencil-square-o" aria-hidden="true"/>{" "}
-                                            Edit User
-                                        </a>
-                                    </td>
-                                    <td>7045</td>
-                                    <td>Xiao Bosco IV</td>
-                                    <td>kristeen.cormier@gmail.com</td>
-                                    <td>Students</td>
-                                    <td>ACTIVE</td>
-                                </tr>
-                                <tr role="row" className="even">
-                                    <td>
-                                        <a
-                                            onClick="Users.edit_user(7044)"
-                                            href="javascript:void(0)"
-                                            className="btn btn-primary btn-sm"
-                                            role="button"
-                                        >
-                                            <i className="fa fa-pencil-square-o" aria-hidden="true"/>{" "}
-                                            Edit User
-                                        </a>
-                                    </td>
-                                    <td>7044</td>
-                                    <td>Winnie Langworth</td>
-                                    <td>donte.douglas@gmail.com</td>
-                                    <td>Librarian</td>
-                                    <td>ACTIVE</td>
-                                </tr>
-                                <tr role="row" className="odd">
-                                    <td>
-                                        <a
-                                            onClick="Users.edit_user(7043)"
-                                            href="javascript:void(0)"
-                                            className="btn btn-primary btn-sm"
-                                            role="button"
-                                        >
-                                            <i className="fa fa-pencil-square-o" aria-hidden="true"/>{" "}
-                                            Edit User
-                                        </a>
-                                    </td>
-                                    <td>7043</td>
-                                    <td>Homer Littel</td>
-                                    <td>nidia.schulist@hotmail.com</td>
-                                    <td>Students</td>
-                                    <td>ACTIVE</td>
-                                </tr>
-                                <tr role="row" className="even">
-                                    <td>
-                                        <a
-                                            onClick="Users.edit_user(7042)"
-                                            href="javascript:void(0)"
-                                            className="btn btn-primary btn-sm"
-                                            role="button"
-                                        >
-                                            <i className="fa fa-pencil-square-o" aria-hidden="true"/>{" "}
-                                            Edit User
-                                        </a>
-                                    </td>
-                                    <td>7042</td>
-                                    <td>Miss Tanna Torphy</td>
-                                    <td>angel.huels@gmail.com</td>
-                                    <td>Librarian</td>
-                                    <td>ACTIVE</td>
-                                </tr>
+                                {
+                                    filteredUsers.map((user, index) => {
+                                        return (
+                                            <tr role="row" className="odd">
+                                                <td>
+                                                    <a
+                                                        href="javascript:;"
+                                                        className="btn btn-xs btn-primary"
+                                                        onClick="Users.edit_user(1);"
+                                                    >
+                                                        <i className="fa fa-edit"/>
+                                                    </a>
+                                                    <a
+                                                        href="javascript:;"
+                                                        className="btn btn-xs btn-danger"
+                                                        onClick="Users.delete_user(1);"
+                                                    >
+                                                        <i className="fa fa-trash"/>
+                                                    </a>
+                                                </td>
+                                                <td className="sorting_1">{user.id}</td>
+                                                <td>{user.full_name}</td>
+                                                <td>{user.email}</td>
+                                                <td>{user.group_name}</td>
+                                                <td>{user.status}</td>
+                                            </tr>
+                                        )
+
+                                    })
+
+                                }
                                 </tbody>
                             </table>
                         </div>
