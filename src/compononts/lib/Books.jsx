c5d2110de3bb795de3ac283f91ef6245fbe56c4f
enderta <endertanver@gmail.com>
2022-11-06 13:38:14 +0000

    car

--- src/compononts/lib/Books.jsx
+++ src/compononts/lib/Books.jsx
@@ -39,28 +39,7 @@ const Books = () => {
         setCount(event.target.value);
    }
 
-   React.useEffect(() => {
-       const myHeaders = new Headers();
-       myHeaders.append("x-library-token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImlkIjoiNTgzNiIsImZ1bGxfbmFtZSI6IlRlc3QgTGlicmFyaWFuIDI0IiwiZW1haWwiOiJsaWJyYXJpYW4yNEBsaWJyYXJ5IiwidXNlcl9ncm91cF9pZCI6IjIifSwiaWF0IjoxNjY3Mzg3NjA1LCJleHAiOjE2Njk5Nzk2MDV9.P8Y0XKzwiZjRA8718Q3pGOaTodEI5rGi9KL_SUbhrm0");
-
-       const requestOptions = {
-           method: 'GET',
-           headers: myHeaders,
-           redirect: 'follow'
-       };
-
-       fetch("https://library2.cydeo.com/rest/v1/get_books?book_categories=null&draw=1&columns%5B0%5D%5Bdata%5D=actions&columns%5B0%5D%5Bname%5D=actions&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=false&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=isbn&columns%5B1%5D%5Bname%5D=isbn&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=name&columns%5B2%5D%5Bname%5D=B.name&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=author&columns%5B3%5D%5Bname%5D=B.author&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=category&columns%5B4%5D%5Bname%5D=BC.name&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=year&columns%5B5%5D%5Bname%5D=B.year&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=full_name&columns%5B6%5D%5Bname%5D=U.full_name&columns%5B6%5D%5Bsearchable%5D=true&columns%5B6%5D%5Borderable%5D=true&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=1&order%5B0%5D%5Bdir%5D=desc&start=0&length=10000&search%5Bvalue%5D=&search%5Bregex%5D=false&_=1667734188443", requestOptions)
-           .then(response => response.json())
-           .then(result => {
-               setCatagory(result.data);
-
-
-           })
-           .catch(error => console.log('error', error));
-   }
-    , []);
-
-  let cats=[...new Set(catagory.map(item=>item.category))];
+    let cats=['Fairy Tale', 'Action and Adventure', 'Classic', 'Anthology', 'Short Story', 'Drama', 'Comic and Graphic Novel', 'Crime and Detective', 'Romance', 'Fantasy', 'Fan-Fiction', 'Science Fiction', 'Historical Fiction', 'Memoir', 'Humor', 'Poetry', 'Fable', 'Essay', 'Horror', 'Biography/Autobiography'];
     console.log(cats);
 
 
