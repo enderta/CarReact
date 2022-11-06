b5a328cadbf2955ab15a2e32b4d6a7f4253325a2
enderta <endertanver@gmail.com>
2022-11-06 12:34:14 +0000

    car

--- src/compononts/lib/AddBook.jsx
+++ src/compononts/lib/AddBook.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const AddBook = () => {
+    const [isbn, setIsbn] = React.useState("");
+    const [title, setTitle] = React.useState("");
+    const [author, setAuthor] = React.useState("");
+    const [catagory, setCatagory] = React.useState("");
+    const [year, setYear] = React.useState("");
+    const [borrowed, setBorrowed] = React.useState("");
     return (
         <div>
             add book
--- src/compononts/lib/Books.jsx
+++ src/compononts/lib/Books.jsx
@@ -4,27 +4,18 @@ import {Link} from "react-router-dom";
 
 const Books = () => {
     const [books, setBooks] = React.useState([]);
-    const [loading, setLoading] = React.useState(true);
     const [search, setSearch] = React.useState("");
     const [filteredBooks, setFilteredBooks] = React.useState([]);
-    const [isbn, setIsbn] = React.useState("");
-    const [title, setTitle] = React.useState("");
-    const [author, setAuthor] = React.useState("");
-    const [catagory, setCatagory] = React.useState("");
-    const [year, setYear] = React.useState("");
-    const [borrowed, setBorrowed] = React.useState("");
     const [count, setCount] = React.useState(50);
 
     React.useEffect(() => {
-        var myHeaders = new Headers();
+        const myHeaders = new Headers();
         myHeaders.append("x-library-token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImlkIjoiNTgzNiIsImZ1bGxfbmFtZSI6IlRlc3QgTGlicmFyaWFuIDI0IiwiZW1haWwiOiJsaWJyYXJpYW4yNEBsaWJyYXJ5IiwidXNlcl9ncm91cF9pZCI6IjIifSwiaWF0IjoxNjY3Mzg3NjA1LCJleHAiOjE2Njk5Nzk2MDV9.P8Y0XKzwiZjRA8718Q3pGOaTodEI5rGi9KL_SUbhrm0");
-
-        var requestOptions = {
+        const requestOptions = {
             method: 'GET',
             headers: myHeaders,
             redirect: 'follow'
         };
-
         fetch(`https://library2.cydeo.com/rest/v1/get_books?book_categories=null&draw=1&columns%5B0%5D%5Bdata%5D=actions&columns%5B0%5D%5Bname%5D=actions&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=false&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=isbn&columns%5B1%5D%5Bname%5D=isbn&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=name&columns%5B2%5D%5Bname%5D=B.name&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=author&columns%5B3%5D%5Bname%5D=B.author&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=category&columns%5B4%5D%5Bname%5D=BC.name&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=year&columns%5B5%5D%5Bname%5D=B.year&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=full_name&columns%5B6%5D%5Bname%5D=U.full_name&columns%5B6%5D%5Bsearchable%5D=true&columns%5B6%5D%5Borderable%5D=true&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=1&order%5B0%5D%5Bdir%5D=desc&start=0&length=${count}&search%5Bvalue%5D=&search%5Bregex%5D=false&_=1667734188443`, requestOptions)
             .then(response => response.json())
             .then(result => {
@@ -33,9 +24,6 @@ const Books = () => {
             }
             )
             .catch(error => console.log('error', error));
-
-
-
     }, [count]);
     const handleSearch = (event) => {
         setSearch(event.target.value);
@@ -58,7 +46,6 @@ const Books = () => {
                 <section className="container">
                     <div className="row">
                         <div className="col-md-12">
-
      <span style={{float: "right"}}>
        <Link className={"btn btn"} to={"/books/add"}><h6>Add Book</h6></Link>
       </span>
