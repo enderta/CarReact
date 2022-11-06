9ed7def271f1688a326a265d7b7c08d639676fb8
enderta <endertanver@gmail.com>
2022-11-06 17:20:24 +0000

    car

--- src/compononts/lib/Nav.jsx
+++ src/compononts/lib/Nav.jsx
@@ -5,10 +5,10 @@ const Nav = () => {
     return (
         <div>
            <Navbar    variant="dark" style={{border:'1px solid',padding:'10px'}}>
-                <Navbar.Brand href="/home">Home </Navbar.Brand>
-                <Navbar.Brand href="/users">Users</Navbar.Brand>
-                <Navbar.Brand href="/books">Books</Navbar.Brand>
-                <Navbar.Brand href="/BorrowedBooks">Borrowed Books</Navbar.Brand>
+                <Navbar.Brand href="/home"><h4>Home </h4></Navbar.Brand>
+                <Navbar.Brand href="/users"><h4>Users</h4></Navbar.Brand>
+                <Navbar.Brand href="/books"><h4>Books</h4></Navbar.Brand>
+                <Navbar.Brand href="/BorrowedBooks"><h4>Borrowed Books</h4></Navbar.Brand>
                 {/*   <Navbar.Brand href="/login">Logout</Navbar.Brand>*/}
 
             </Navbar>
