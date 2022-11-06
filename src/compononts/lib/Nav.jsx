3ee361b66563b751f857957ee6253496aeef61cd
enderta <endertanver@gmail.com>
2022-11-06 17:21:39 +0000

    car

--- src/compononts/lib/Nav.jsx
+++ src/compononts/lib/Nav.jsx
@@ -4,7 +4,7 @@ import {Navbar} from "react-bootstrap";
 const Nav = () => {
     return (
         <div>
-           <Navbar    variant="dark" style={{border:'1px solid',padding:'10px'}}>
+           <Navbar    variant="dark" style={{border:'1px solid',padding:'10px',margin:"10px"}}>
                 <Navbar.Brand href="/home"><h4>Home </h4></Navbar.Brand>
                 <Navbar.Brand href="/users"><h4>Users</h4></Navbar.Brand>
                 <Navbar.Brand href="/books"><h4>Books</h4></Navbar.Brand>
