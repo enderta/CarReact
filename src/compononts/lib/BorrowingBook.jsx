5a3f04cf7140c6b453cb59de454a8bc8bf747080
enderta <endertanver@gmail.com>
2022-11-08 11:38:06 +0000

    car

--- src/compononts/lib/BorrowingBook.jsx
+++ src/compononts/lib/BorrowingBook.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import {useParams} from "react-router";
 
 const BorrowingBook = () => {
+    const [loading, setLoading] = React.useState(true);
+    const [data, setData] = React.useState([]);
+    let param = useParams();
+    React.useEffect(() => {
+        const myHeaders = new Headers();
+        myHeaders.append("x-library-token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImlkIjoiNTgzNiIsImZ1bGxfbmFtZSI6IlRlc3QgTGlicmFyaWFuIDI0IiwiZW1haWwiOiJsaWJyYXJpYW4yNEBsaWJyYXJ5IiwidXNlcl9ncm91cF9pZCI6IjIifSwiaWF0IjoxNjY3Mzg3NjA1LCJleHAiOjE2Njk5Nzk2MDV9.P8Y0XKzwiZjRA8718Q3pGOaTodEI5rGi9KL_SUbhrm0");
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "book_id": param.id,
+            "user_id": 1
+        });
+
+        const requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch("https://library2.cydeo.com/rest/v1/book_borrow", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                setData(result);
+                setLoading(false);
+            }
+            )
+    }, []);
     return (
         <div>
-            
+            {loading ? <div><h3>Borrowing...</h3></div> : <div><h3>{data.message}</h3></div>}
+            {setTimeout(() => {
+                window.location.href = "/borrowedBooks";}
+            , 3000)}
         </div>
     );
 };
