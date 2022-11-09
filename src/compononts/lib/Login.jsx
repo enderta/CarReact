400bc27907dcaac170914645b5a9518bfd3d3855
enderta <endertanver@gmail.com>
2022-11-09 11:28:13 +0000

    car

--- src/compononts/lib/Login.jsx
+++ src/compononts/lib/Login.jsx
@@ -1,36 +1,58 @@
 import React from 'react';
 import {Button, Image} from "react-bootstrap";
-import {Link} from "react-router-dom";
 
 const Login = () => {
+    const [email, setEmail] = React.useState("")
+    const [password, setPassword] = React.useState("")
+
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+    }
+    const handleLogin = () => {
+        if (email === "admin@gmail" && password === "admin") {
+            window.location.href = "/home"
+        } else {
+            alert("Invalid email or password")
+        }
+    }
 
     return (
         <div>
             <div>
-                <Image src={require('./pexels-ersan-13864601.jpg')} style={{position:'absolute',opacity:'0.3',height:"100%",width:"100%"}}/>
+                <Image src={require('./pexels-ersan-13864601.jpg')}
+                       style={{position: 'absolute', opacity: '0.3', height: "100%", width: "100%"}}/>
             </div>
-            <div className="container" >
+            <div className="container">
                 <div className="row">
                     <div className="col-6">
-                        <div className="card" style={{border:"1px solid",padding:'10px',margin:"10px",position:'absolute',top:'25%',left:"25%",right:"25%"}}>
-                            <div className="card-body" >
+                        <div className="card" style={{
+                            border: "1px solid",
+                            padding: '10px',
+                            margin: "10px",
+                            position: 'absolute',
+                            top: '25%',
+                            left: "25%",
+                            right: "25%"
+                        }}>
+                            <div className="card-body">
                                 <h1 className="card-title">Birmingham Library</h1>
-                                <form >
-                                    <div className="form-group" >
-                                        <label htmlFor="exampleInputEmail1">Email</label>
+                                <form>
+                                    <div className="form-group">
+                                        <label htmlFor="exampleInputEmail1">Email </label>
                                         <input type="email" className="form-control" id="exampleInputEmail1"
-                                               aria-describedby="emailHelp"/>
+                                                  aria-describedby="emailHelp" onChange={handleEmail}/>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="exampleInputPassword1">Password</label>
-                                        <input type="password" className="form-control" id="exampleInputPassword1"/>
+                                        <input type="password" className="form-control" id="exampleInputPassword1"
+                                                    onChange={handlePassword}/>
                                     </div>
-                                    <br/>
-                                    <Link to='/home'>
-                                        <Button className={"btn btn-sm btn-outline btn-danger"}>
-                                            Login
-                                        </Button>
-                                    </Link>
+                                   <br/>
+                                    <Button variant="primary" onClick={handleLogin}>Login</Button>
                                 </form>
                             </div>
                         </div>
@@ -40,7 +62,6 @@ const Login = () => {
         </div>
     );
 }
-
 
 
 export default Login;
