5e2dcd8189520f6a2b751cf977e17f478aed50c0
enderta <endertanver@gmail.com>
2022-11-28 20:44:16 +0000

    health

--- src/compononts/moives-series/MoviesSeries.jsx
+++ src/compononts/moives-series/MoviesSeries.jsx
@@ -12,11 +12,7 @@ const MoviesSeries = () => {
         <ScrollToTop />
         <Routes>
             <Route path="/" element={<HomeNavi/>}/>
-
-            <Route path="/series" element={<Series/>}>
-                
-
-            </Route>
+            <Route path="/series" element={<Series/>}/>
             <Route path="/series/:id" element={<Seasons/>}/>
             <Route path='/movies' element={<Movies/>}/>
         </Routes>
