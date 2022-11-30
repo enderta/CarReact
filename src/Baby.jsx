15c8cca9d36080ebadbc647cd86d15a161d94632
enderta <endertanver@gmail.com>
2022-11-30 18:19:30 +0000

    health

--- src/Baby.jsx
+++ src/Baby.jsx
@@ -5,35 +5,121 @@ import './baby.css'
 const Baby = () => {
     //sort the array by name in ascending order object
     const sortedByName = demo.sort((a, b) => {
-        if (a.name > b.name) {
-            return 1;
-        } else if (a.name < b.name) {
-            return -1;
-        } else {
-            return 0;
+            if (a.name > b.name) {
+                return 1;
+            } else if (a.name < b.name) {
+                return -1;
+            } else {
+                return 0;
+            }
         }
-    }
     );
 
-    console.log(sortedByName);
+    const [search, setSearch] = React.useState('');
+    const [filtered, setFiltered] = React.useState(sortedByName);
+    const [favorite, setFavorite] = React.useState([]);
+
+    const handleChange = (e) => {
+        setSearch(e.target.value);
+        setFiltered(sortedByName.filter((item) => item.name.toLowerCase().includes(e.target.value.toLowerCase())));
+    }
+
+    const addFavorite = (item) => {
+        setFavorite([...favorite, item]);
+        setFiltered(filtered.filter((i) => i.id !== item.id).sort((a, b) => {
+                if (a.name > b.name) {
+                    return 1;
+                } else if (a.name < b.name) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        ));
+    }
+    const removeFavorite = (item) => {
+        setFavorite(favorite.filter((i) => i.id !== item.id));
+        setFiltered([...filtered, item].sort((a, b) => {
+            if (a.name > b.name) {
+                return 1;
+            } else if (a.name < b.name) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }));
+    }
+
     return (
         <div>
-       <div>
-           {
-                sortedByName.map((a)=>{
-                    return(
-                        <div className="baby">
-                            {a.sex==='f'? <h1 style={{color:'pink'}}>{a.name}</h1>:<h1 style={{color:'blue'}}>{a.name}</h1>}
+            <div>
+                <input type="text" placeholder="Search" value={search} onChange={handleChange}/>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-6">
+                        <h1>Favorite</h1>
+                        <ul>
+                            {
+                                favorite.map((item, index) => {
+                                        return (
+                                            <li key={index} className={item.sex} onClick={() => removeFavorite(item)}>
+                                                {
+                                                    item.sex === 'm' ?
+                                                        <button style={{
+                                                            color: 'blue',
+                                                            border: "1px solid"
+                                                        }}>{item.name}</button>
+                                                        :
+                                                        <button
+                                                            style={{color: 'red', border: "1px solid"}}>{item.name}</button>
+                                                }
+                                            </li>
+                                        )
+                                    }
+                                )
+                            }
+                            <br/>
+                            <br/>
+                        </ul>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-6">
+                                    <div>
+                                        <h1>Names</h1>
+                                        <ul>
+                                            {
+                                                filtered.map((item, index) => {
+                                                    return (
+                                                        <li className={item.sex} key={index}
+                                                            onClick={() => addFavorite(item)}>
+                                                            {
+                                                                item.sex === 'm' ?
+                                                                    <button style={{
+                                                                        color: 'blue',
+                                                                        border: "1px solid"
+                                                                    }}>{item.name}</button>
+                                                                    :
+                                                                    <button style={{
+                                                                        color: 'red',
+                                                                        border: "1px solid"
+                                                                    }}>{item.name}</button>
+
+                                                            }
+                                                        </li>
+                                                    )
+                                                })
+                                            }
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
-                    )
-
-                }
-                )
-           }
-       </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    );
-
-};
+    )
+}
 
 export default Baby;
