119bd39edb965c755c855ae9793dba89c6b65dba
enderta <endertanver@gmail.com>
2022-12-05 12:36:04 +0000

    flags

--- src/compononts/countries/TableCon.jsx
+++ src/compononts/countries/TableCon.jsx
@@ -12,14 +12,17 @@ const TableCon = (props) => {
 
 
             <>
-                <div className="col-md-4" >
-                    <div className="card mb-4 " style={{background:"black",border:"#D43C31 solid",margin:'2px',padding:'2px'}}>
-                        <img src={props.item.flag} style={{"height":"500px",'weihgt':"200px"}} className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5  style={{color:"green"}} className="card-title">Name: {props.item.name}</h5>
-                            <p style={{color:"green"}} className="card-text">Capital: {props.item.capital}</p>
+                <div className="col-md-3" >
+                    <div className="card mb-4 shadow-sm" style={{"height":"400px",'weihgt':"400px",background:"black",border:"#D43C31 solid",margin:'2px',padding:'2px'}}>
+                        <img src={props.item.flag} style={{"height":"400px",'weihgt':"400px"}} className="card-img-top" />
+                        <div style={{color:"green"}}  className="card-body">
+                            <p style={{color:"green"}}  className="card-text">{props.item.name}</p>
+                            <p style={{color:"green"}}  className="card-text">{props.item.capital}</p>
+                            <div className="d-flex justify-content-between align-items-center">
+                            </div>
+                            <Button variant="primary" onClick={handleShow}>Details</Button>
+
                         </div>
-                        <Button variant="primary" onClick={handleShow}>Go somewhere</Button>
                         <ModalCon show={show} setShow={setShow} item={props.item}/>
                     </div>
                 </div>
