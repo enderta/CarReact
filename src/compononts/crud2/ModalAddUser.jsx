e62d88debb75f282e9a65a9d9f7c50b349bc3f1c
enderta <endertanver@gmail.com>
2022-12-14 16:47:17 +0000

    crud2

--- src/compononts/crud2/ModalAddUser.jsx
+++ src/compononts/crud2/ModalAddUser.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
-import {Button, Modal} from "react-bootstrap";
+import {Button, Form, Modal} from "react-bootstrap";
 
 const ModalAddUser = (props) => {
+    const [name, setName] = React.useState('');
+    const [surname, setSurname] = React.useState('');
+    const [title, setTitle] = React.useState('');
+    const handleChange=(e)=>{
+        if(e.target.name==='name'){
+            setName(e.target.value);
+        }
+        if(e.target.name==='surname'){
+            setSurname(e.target.value);
+        }
+        if(e.target.name==='title'){
+            setTitle(e.target.value);
+        }
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const user = {
+            name,
+            surname,
+            title
+        }
+        fetch('http://localhost:3001/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        }).then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => console.log(err))
+        window.location.href = 'http://localhost:3000';
+    }
     return (
         <div>
             <Modal show={props.show} onHide={() => props.setShow(false)}>
@@ -9,16 +43,25 @@ const ModalAddUser = (props) => {
                     <Modal.Title>Add User</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <h1>Modal body text goes here.</h1>
+                    <Form >
+                        <Form.Group className="mb-2" controlId="formBasicEmail" >
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter name" name={'name'} value={name} onChange={handleChange}/>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Surname</Form.Label>
+                            <Form.Control type="text" placeholder="Enter surname" name={'surname'} value={surname} onChange={handleChange}/>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control type="text" placeholder="Enter title" name={'title'} value={title} onChange={handleChange}/>
+                        </Form.Group>
+                        <Button variant="primary" type="submit" onClick={handleSubmit}>
+                            Submit
+                        </Button>
+                    </Form>
                 </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={() => props.setShow(false)}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={() => props.setShow(false)}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
+
             </Modal>
 
 
