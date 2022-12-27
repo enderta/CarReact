a1b8e9a31f40a4bfad74a45b275b7ea759c52306
enderta <endertanver@gmail.com>
2022-12-27 17:07:17 +0000

    notes

--- src/compononts/carNode/LoginReg.jsx
+++ src/compononts/carNode/LoginReg.jsx
@@ -3,11 +3,12 @@ import '../car/car.css';
 import {Form, FormGroup} from "react-bootstrap";
 import Cars from "./Cars";
 import Owners from "./Owners";
+
 const LoginReg = () => {
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
     const [err, setErr] = React.useState(false);
-    let token='';
+    let token = '';
 
 
     const handleEmail = (e) => {
@@ -16,125 +17,135 @@ const LoginReg = () => {
     const handlePassword = (e) => {
         setPassword(e.target.value);
     }
-   const login= (e) => {
+    const login = (e) => {
         e.preventDefault()
-    const user = {
-        email,
-        password
-    }
-    fetch('http://localhost:3001/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    }).then(res => res.json())
-        .then(data => {
-            if(data.token){
-               localStorage.setItem('token', data.token);
-                window.location.href = 'http://localhost:3000/home';
-            }
-            else {
-                alert(data.message);
-
-            }
+        const user = {
+            email,
+            password
         }
-        )
-        .catch(err => console.log(err))
+        if (email === '' || password === '') {
+            alert('Please enter all fields');
+        } else {
+            fetch('http://localhost:3001/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(user)
+            }).then(res => res.json())
+                .then(data => {
+                        if (data.token) {
+                            localStorage.setItem('token', data.token);
+                            window.location.href = 'http://localhost:3000/home';
+                        } else {
+                            alert(data.message);
+
+                        }
+                    }
+                )
+                .catch(err => console.log(err))
+        }
 
     }
 
 
-   const register = (e) => {
+
+    const register = (e) => {
         e.preventDefault()
-    const user = {
-        email,
-        password
-    }
-    fetch('http://localhost:3001/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    }).then(res => res.json())
-        .then(data => {
-            if(data.message==='User created'){
-
-                window.location.href = 'http://localhost:3000/login';
-            }
-            else {
-                alert(data.message);
-
-            }
+        const user = {
+            email,
+            password
         }
-        )
-        .catch(err => console.log(err))
+        if (email === '' || password === '') {
+            alert("Please fill in all the fields");
+        } else {
+            fetch('http://localhost:3001/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(user)
+            }).then(res => res.json())
+                .then(data => {
+                        if (data.message === 'User created') {
 
+                            window.location.href = 'http://localhost:3000/login';
+                        } else {
+                            alert(data.message);
+
+                        }
+                    }
+                )
+                .catch(err => console.log(err))
+
+        }
     }
 
 
-
-    return (
-        <div>
+        return (
             <div>
-                <video autoPlay loop muted>
-                    <source
-                        src={require('../car/pexels-anthony-shkraba-8064146.mp4')}
-                        type="video/mp4"
-                    />
+                <div>
+                    <video autoPlay loop muted>
+                        <source
+                            src={require('../car/pexels-anthony-shkraba-8064146.mp4')}
+                            type="video/mp4"
+                        />
 
-                </video>
+                    </video>
 
-                <div className="container">
-                    <div className="row">
-                        <div className="col-6">
-                            <div className="card" style={{
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-6">
+                                <div className="card" style={{
 
-                                padding: '10px',
-                                margin: "10px",
-                                position: 'absolute',
-                                top: '25%',
-                                left: "25%",
-                                right: "25%",
-                                background: "none",
-                                border: "none"
-                            }}>
-                                <div className="card-body">
+                                    padding: '10px',
+                                    margin: "10px",
+                                    position: 'absolute',
+                                    top: '25%',
+                                    left: "25%",
+                                    right: "25%",
+                                    background: "none",
+                                    border: "none"
+                                }}>
+                                    <div className="card-body">
 
-                                    <FormGroup>
-                                        <Form>
-                                            <div className="form-group">
-                                                <label htmlFor="exampleInputEmail1" style={{color:"#f3ce13"}}>Email </label>
-                                                <input  type="email" className="form-control" id="exampleInputEmail1"
-                                                        required={
-                                                            true
-                                                        } aria-describedby="emailHelp" onChange={handleEmail}/>
-                                            </div>
-                                            <div className="form-group">
-                                                <label style={{color:"#f3ce13"}} htmlFor="exampleInputPassword1">Password</label>
-                                                <input type="password" className="form-control" id="exampleInputPassword1"
-                                                     required={
-                                                            true
-                                                     }  onChange={handlePassword}/>
+                                        <FormGroup>
+                                            <Form>
+                                                <div className="form-group">
+                                                    <label htmlFor="exampleInputEmail1"
+                                                           style={{color: "#f3ce13"}}>Email </label>
+                                                    <input type="email" className="form-control" id="exampleInputEmail1"
+                                                           required={
+                                                               true
+                                                           } aria-describedby="emailHelp" onChange={handleEmail}/>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label style={{color: "#f3ce13"}}
+                                                           htmlFor="exampleInputPassword1">Password</label>
+                                                    <input type="password" className="form-control"
+                                                           id="exampleInputPassword1"
+                                                           required={
+                                                               true
+                                                           } onChange={handlePassword}/>
 
-                                            </div>
-                                            <br/>
-                                            <button className="btn btn-success" onClick={login} style={{
-                                                margin: "10px",
-                                            }}>Login</button>
-                                            <button className="btn btn-info" onClick={register}>Register</button>
-                                        </Form>
-                                    </FormGroup>
+                                                </div>
+                                                <br/>
+                                                <button className="btn btn-success" onClick={login} style={{
+                                                    margin: "10px",
+                                                }}>Login
+                                                </button>
+                                                <button className="btn btn-info" onClick={register}>Register</button>
+                                            </Form>
+                                        </FormGroup>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-    );
-};
+        );
+    };
 
-export default LoginReg;
+    export default LoginReg;
