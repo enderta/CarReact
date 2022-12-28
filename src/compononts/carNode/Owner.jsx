bfd1480072e3464efdda2c5e1b49704b556f519d
enderta <endertanver@gmail.com>
2022-12-28 15:10:54 +0000

    notes

--- src/compononts/carNode/Owner.jsx
+++ src/compononts/carNode/Owner.jsx
@@ -4,9 +4,12 @@ import AddCars from "./AddCars";
 
 const Owner = (props) => {
     const [show, setShow] = React.useState(false);
+    const [show2, setShow2] = React.useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const handleClose2 = () => setShow2(false);
+    const handleShow2 = () => setShow2(true);
 
     const handleDelete = (id) => {
         fetch(`http://localhost:3001/owners/${id}`, {
@@ -31,9 +34,9 @@ const Owner = (props) => {
                 <td>{props.owner.email}</td>
                 <td>
                     <button className="btn btn-outline-info" style={{margin:"10px"}} onClick={handleShow}>Cars</button>
-                    <button className="btn btn-outline-success" >Add Cars</button>
+                    <button className="btn btn-outline-success" onClick={handleShow2} >Add Cars</button>
                     <Cars id={props.owner.id} show={show} handleClose={handleClose}/>
-                    <AddCars id={props.owner.id} show={show} handleClose={handleClose}/>
+                    <AddCars id={props.owner.id} show={show2} handleClose={handleClose2}/>
 
 
                 </td>
