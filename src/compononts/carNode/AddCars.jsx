5451866ce920c1c44c4f02092f5cffb9cbcc4dce
enderta <endertanver@gmail.com>
2022-12-28 15:05:23 +0000

    notes

---
+++ src/compononts/carNode/AddCars.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddCars = (props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default AddCars;
--- src/compononts/carNode/Owner.jsx
+++ src/compononts/carNode/Owner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Cars from "./Cars";
+import AddCars from "./AddCars";
 
 const Owner = (props) => {
     const [show, setShow] = React.useState(false);
@@ -29,8 +30,12 @@ const Owner = (props) => {
                 <td>{props.owner.surname}</td>
                 <td>{props.owner.email}</td>
                 <td>
-                    <button className="btn btn-outline-info" onClick={handleShow}>Cars</button>
+                    <button className="btn btn-outline-info" style={{margin:"10px"}} onClick={handleShow}>Cars</button>
+                    <button className="btn btn-outline-success" >Add Cars</button>
                     <Cars id={props.owner.id} show={show} handleClose={handleClose}/>
+                    <AddCars id={props.owner.id} show={show} handleClose={handleClose}/>
+
+
                 </td>
                 <td>
                     <button className="btn btn-outline-danger" onClick={() => handleDelete(props.owner.id)}>Delete</button>
