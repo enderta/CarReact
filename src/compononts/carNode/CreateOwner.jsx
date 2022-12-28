61b01d1b87ab4dfd9e12b73fd30409e436d852b9
enderta <endertanver@gmail.com>
2022-12-28 14:22:36 +0000

    notes

---
+++ src/compononts/carNode/CreateOwner.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {Modal, ModalBody} from "react-bootstrap";
+
+const CreateOwner = (props) => {
+    const [name, setName] = React.useState('');
+    const [surname, setSurname] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const handleChange=(e)=>{
+        if(e.target.name==='name'){
+            setName(e.target.value);
+        }
+        if(e.target.name==='surname'){
+            setSurname(e.target.value);
+        }
+        if(e.target.name==='email'){
+            setEmail(e.target.value);
+        }
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const user = {
+            name,
+            surname,
+            email
+        }
+            fetch('http://localhost:3001/owners', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem('token')
+                },
+                body: JSON.stringify(user)
+            }).then(res => res.json())
+                .then(data => {
+                    props.setOwners([...props.owners, data]);
+                }
+                )
+                .catch(err => console.log(err))
+            window.location.reload();
+    }
+
+    return (
+        <div>
+            <Modal show={props.show} onHide={props.handleClose}   >
+                <ModalBody>
+                    <form >
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" className="form-control" id="name" name={'name'} value={name} onChange={handleChange}/>
+                            <label htmlFor="surname">Surname</label>
+                            <input type="text" className="form-control" id="surname" name={'surname'} value={surname} onChange={handleChange}/>
+                            <label htmlFor="email">Email</label>
+                            <input type="text" className="form-control" id="email" name={'email'} value={email} onChange={handleChange}/>
+                        </div>
+                        <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Submit</button>
+                    </form>
+                </ModalBody>
+            </Modal>
+
+
+        </div>
+    );
+};
+
+export default CreateOwner;
--- src/compononts/carNode/Owners.jsx
+++ src/compononts/carNode/Owners.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Owner from "./Owner";
-import {Table} from "react-bootstrap";
+import {Modal, Table} from "react-bootstrap";
+import CreateOwner from "./CreateOwner";
 
 const Owners = (props) => {
     const [owners, setOwners] = React.useState([]);
+    const [show, setShow] = React.useState(false);
+
 
     React.useEffect(() => {
         fetch('http://localhost:3001/owners',
@@ -17,39 +20,51 @@ const Owners = (props) => {
             })
             .then(response => response.json())
             .then(data => {
-                setOwners(data);
-            }
+                    setOwners(data);
+                }
             );
     }, []);
     const handleLogout = () => {
         localStorage.removeItem('token');
         window.location.href = 'http://localhost:3000/';
     }
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <div>
-            <h1 style={{color:"yellow" ,textAlign:"center"}} >Owners</h1>
+            <h1 style={{color: "yellow", textAlign: "center"}}>Owners</h1>
             <div>
-                  <span >
-                <button onClick={handleLogout} className="btn btn-outline-info" style={{float:"right" ,margin:"10px"}}>Logout</button>
+                  <span>
+
+                <button onClick={handleLogout} className="btn btn-outline-info"
+                        style={{float: "right", margin: "10px"}}>Logout</button>
+                        <button className="btn btn-outline-info" onClick={handleShow}
+                                style={{float: "right", margin: "10px"}}>Create Owner</button>
+
+                          <CreateOwner owners={owners} setOwners={setOwners} show={show} handleClose={handleClose}/>
+
+
             </span>
             </div>
 
 
             <div className="container">
-            <table className="table table-responsive table-dark table-borderless table-striped table-bordered table-hover">
-                <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>First Name</th>
-                    <th>Surname</th>
-                    <th>Email</th>
-                    <th>Cars</th>
-                </tr>
-                </thead>
-                <tbody>
-                {owners.map(owner => <Owner key={owner.id} owner={owner}/>)}
-                </tbody>
-            </table>
+                <table
+                    className="table table-responsive table-dark table-borderless table-striped table-bordered table-hover">
+                    <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>First Name</th>
+                        <th>Surname</th>
+                        <th>Email</th>
+                        <th>Cars</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {owners.map(owner => <Owner key={owner.id} owner={owner}/>)}
+                    </tbody>
+                </table>
 
             </div>
         </div>
