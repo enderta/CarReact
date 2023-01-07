f3bbbede105840cf98957b54039bd20df1b80f6d
enderta <endertanver@gmail.com>
2023-01-07 17:08:05 +0000

    city

--- src/compononts/guide/MainCity.jsx
+++ src/compononts/guide/MainCity.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import DropDown from "./DropDown.jsx";
 
 const MainCity = () => {
-    const city=["Harrow","Heatrow","Startford"];
-    const category=["pharmcies","doctors","colleges","hospitals"];
-
 
     return (
         <div>
