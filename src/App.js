c0b916b2cfb0811571774e776914903669ab493b
enderta <endertanver@gmail.com>
2023-01-23 12:56:16 +0000

    city

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React  from 'react';
-import FAQAccrod from "./compononts/userCrud/FAQAccrod.jsx";
-import Quote from "./compononts/userCrud/Quote.jsx";
-import ShoppingList from "./compononts/userCrud/ShoppingList.jsx";
 import Videos from "./compononts/video/Videos.jsx";
-import Main from "./compononts/video/video2/Main.jsx";
 
-function FAQAccordion() {
-    return null;
-}
+
+
 
 const App = () => {
 
     return (
         <div>
-    <Main/>
+    <Videos/>
         </div>
 
     );
