5d47d05ec761c17c12aa9894ea2110e05be762b7
enderta <endertanver@gmail.com>
2023-02-02 15:05:53 +0000

    city

--- src/compononts/houses/NavBarS.jsx
+++ src/compononts/houses/NavBarS.jsx
@@ -14,19 +14,18 @@ const NavBarS = (props) => {
                 <a href="#" className="navbar-brand d-flex align-items-center">
                     <strong>Home Owners</strong>
                     <br/>
-                   <span>
-                        <form className={"d-flex"} style={{margin:'10px'}}>
-                        <input className={"form-control me-2"} type={"search"} placeholder={"Search"} aria-label={"Search"} onChange={props.search}/>
+                    <span>
+                        <form className={"d-flex"} style={{margin: '10px'}}>
+                        <input className={"form-control me-2"} type={"search"} placeholder={"Search"}
+                               aria-label={"Search"} onChange={props.search}/>
                     </form>
                    </span>
                 </a>
-                <button type="button" className="btn btn-sm btn-outline-secondary" onClick={handleShow}>Add Owner</button>
+                <button type="button" className="btn btn-sm btn-outline-secondary" onClick={handleShow}>Add Owner
+                </button>
                 <Modal show={show} onHide={handleClose}>
-                    <AddOwner show={show} handleClose={handleClose}  />
+                    <AddOwner show={show} handleClose={handleClose}/>
                 </Modal>
-
-
-
             </div>
 
         </div>
