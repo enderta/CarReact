bf55a1edea1739627dfa4231e4abc10139acc2a6
enderta <endertanver@gmail.com>
2023-02-02 14:57:24 +0000

    city

--- src/compononts/houses/AddHome.jsx
+++ src/compononts/houses/AddHome.jsx
@@ -6,11 +6,11 @@ const AddHome = (props) => {
     const [forsale, setForsale] = React.useState('');
 
     const handleChange = (e) => {
-        if(e.target.name === 'postcode'){
+        if (e.target.name === 'postcode') {
             setPostcode(e.target.value);
-        }else if(e.target.name === 'price'){
+        } else if (e.target.name === 'price') {
             setPrice(e.target.value);
-        }else if(e.target.name === 'forsale'){
+        } else if (e.target.name === 'forsale') {
             setForsale(e.target.value);
         }
     }
@@ -42,11 +42,14 @@ const AddHome = (props) => {
             <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="postcode">Postcode</label>
-                    <input type="text" className="form-control" id="postcode" name={'postcode'} value={postcode} onChange={handleChange}/>
+                    <input type="text" className="form-control" id="postcode" name={'postcode'} value={postcode}
+                           onChange={handleChange}/>
                     <label htmlFor="price">Price</label>
-                    <input type="text" className="form-control" id="price" name={'price'} value={price} onChange={handleChange}/>
+                    <input type="text" className="form-control" id="price" name={'price'} value={price}
+                           onChange={handleChange}/>
                     <label htmlFor="forsale">For Sale</label>
-                    <input type="text" className="form-control" id="forsale" name={'forsale'} value={forsale} onChange={handleChange}/>
+                    <input type="text" className="form-control" id="forsale" name={'forsale'} value={forsale}
+                           onChange={handleChange}/>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
